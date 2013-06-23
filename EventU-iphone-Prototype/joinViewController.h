2b6fef9ee6afa390d2b216b08137652a36996082
Dongjin Wang <wang193@illinois.edu>
2013-06-23 15:13:47 -0700

    First Commit

---
+++ EventU-iphone-Prototype/joinViewController.h
@@ -0,0 +1,13 @@
+//
+//  joinViewController.h
+//  EventU-iphone-Prototype
+//
+//  Created by Dongjin Wang on 6/22/13.
+//  Copyright (c) 2013 Dongjin Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface joinViewController : UIViewController
+
+@end
---
+++ EventU-iphone-Prototype/joinViewController.m
@@ -0,0 +1,38 @@
+//
+//  joinViewController.m
+//  EventU-iphone-Prototype
+//
+//  Created by Dongjin Wang on 6/22/13.
+//  Copyright (c) 2013 Dongjin Wang. All rights reserved.
+//
+
+#import "joinViewController.h"
+
+@interface joinViewController ()
+
+@end
+
+@implementation joinViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
